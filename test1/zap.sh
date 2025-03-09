1a0cdd736a47476aeb0ab637d2292c68fb5dc345
MKny11 <129013947+MKny11@users.noreply.github.com>
2025-03-09 15:33:21 +0000

    Delete test1 directory

--- test1/zap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Démarrer ZAP en mode démon, écouter sur le port 8081
-# Le mode démon permet à ZAP de fonctionner en arrière-plan sans bloquer le terminal
-"C:\Program Files\ZAP\Zed Attack Proxy\zap.sh" -daemon -port 8081
-
